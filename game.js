e6c0a7f9bdd38b33d237f51e84b78119a766040e
Praj41 <pcbhamare.41@gmail.com>
2019-11-09 12:42:02 +0530

    start

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var canvas;
 var canvasContext;
 var posx1 = -200;
-var posy1 = -200;
+var posy1 = 300;
 
 window.onload = function () {
     console.log("Hello");
@@ -12,17 +12,22 @@ window.onload = function () {
 }
 
 function draw() {
-    posx1 = posx1 + 1 ;
-    posy1 = posy1 + 1 ;
-    if(posx1 >= 800) {
+    posx1 = posx1 + 2 ;
+    //if(posx1 >= 350)
+    //posy1 = posy1 + 1 ;
+    if(posx1 >= 1130) {
         posx1 = -200;
-        posy1 = -200;
+        posy1 = 300;
     }
-    console.log(posx1);
+    console.log(posx1 , posx1 - 110 , posx1 - 220 , posx1 - 330);
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
     canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(posx1, 300, 200, 100);
+    canvasContext.fillRect(posx1, posy1, 100, 50);
     canvasContext.fillStyle = 'green';
-    canvasContext.fillRect(350, posy1, 100, 200);
+    canvasContext.fillRect(posx1 - 110, posy1, 100, 50);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(posx1 - 220, posy1, 100, 50);
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(posx1 - 330, posy1, 100, 50);
 }
